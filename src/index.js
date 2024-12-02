d7af3d1cd50f529a3cb4daaa6a1561ac8245d01b
Claireandoscar <clairepatriciacampbell@gmail.com>
2024-12-02 17:37:48 +0000

    second attempt to connect to microsoft clarity

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import clarity from '@microsoft/clarity';
 import './index.css';
 import App from './App';
 import AnalyticsProvider from './components/Analytics/Provider';
+
+clarity.init('p7rboopwor');
 
 const container = document.getElementById('root');
 const root = createRoot(container);
